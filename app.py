605f312e79cac9997f69eab1f4711579b4f79368
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2025-05-06 10:55:09 +0700

    Frontend: Menambahkan Copyright di Footer

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import current_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5500)
+    app.run(host="0.0.0.0", port=5500, debug=False, use_reloader=False)
 
 # if __name__ == '__main__':
 #     app.run(debug=True, port=5500)
